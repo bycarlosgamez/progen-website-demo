1d59ba30af806a641e29b169daf5fa0acd9512b1
Carlos Gámez <hardpx@gmail.com>
2022-08-17 19:08:55 -0500

    added slider and minor changes to nav and fo oter

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
+// nav
+
 ((d) => {
-    const $btnMenu = d.querySelector('.menu-btn');
-    const $menu = d.querySelector('.menu');
-     
-    $btnMenu.addEventListener('click', e => {
-        $btnMenu.firstElementChild.classList.toggle('none');
-        $btnMenu.lastElementChild.classList.toggle('none');
-        $menu.classList.toggle('is-active');
-    })
+  const $btnMenu = d.querySelector(".menu-btn");
+  const $menu = d.querySelector(".menu");
 
-    d.addEventListener('click', e => {
-        if(!e.target.matches('.menu a')) return false;
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
 
-        $btnMenu.firstElementChild.classList.remove('none');
-        $btnMenu.lastElementChild.classList.add('none');
-        $menu.classList.remove('is-active');
-    })
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
 })(document);
+
+// slider
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: false,
+  dots: true,
+  centerModer: true,
+  arrows: false,
+  dots: true,
+  responsive: {
+    480: {
+      dots: true, // dots enabled 480px and up
+    },
+  },
+});
